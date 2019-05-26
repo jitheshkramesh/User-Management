f972fd4ffe5d92664e1d1d456bdf99207d68d8f7
JITHESH <jitheshkramesh@hotmail.com>
2019-05-26 12:12:18 +0400

    first commit

---
+++ UserManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserManagementSystem.Models;
+
+namespace UserManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        USERMANAGEMENTEntities db = new USERMANAGEMENTEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult AddUser(int? id)
+        {
+            // Usermanagement
+            ViewBag.latitude = 52.3800447;
+            ViewBag.longitude = 9.728811599999972;
+            ViewBag.MapApi = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(FormCollection fc, HttpPostedFileBase photo, HttpPostedFileBase document) {
+            if (ModelState.IsValid) {
+                UserDetail user= new UserDetail();
+
+                user.FirstName = fc["FirstName"];
+                user.LastName = fc["LastName"];
+                user.Address = fc["Address"];
+                //user.FileAddress = fc["FileAddress"];
+
+                user.CityName = fc["CityName"];
+                user.Latitude = decimal.Parse(fc["Latitude"]);
+                user.Longitude = decimal.Parse(fc["Longitude"]);
+                user.Description = fc["Description"];
+                //user.PhotoPath = fc["PhotoPath"];
+                user.RegisterDate = DateTime.Now.ToLocalTime();
+                user.mapAddress = fc["map"];
+
+
+                if (photo != null && photo.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(photo.FileName);
+                    string imgpath = Path.Combine(Server.MapPath("~/Files/Images/"), DateTime.Now.ToLongDateString() + fileName);
+                    photo.SaveAs(imgpath);
+                    user.PhotoPath = DateTime.Now.ToLongDateString() + fileName;
+                }
+
+                if (document != null && document.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(document.FileName);
+                    string imgpath = Path.Combine(Server.MapPath("~/Files/Images/"), DateTime.Now.ToLongDateString() + fileName);
+                    document.SaveAs(imgpath);
+                    user.FileAddress = DateTime.Now.ToLongDateString() + fileName;
+                }
+
+                db.UserDetails.Add(user);
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult UserList()
+        {
+
+            return View(db.UserDetails.ToList());
+        }
+    }
+}
---
+++ UserManagementSystem/Models/tblUserDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserManagementSystem.Models
+{
+    public class tblUserDetails
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string FilePath { get; set; }
+        public string ImagePath { get; set; }
+        public string CityName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Description { get; set; }
+    }
+}
